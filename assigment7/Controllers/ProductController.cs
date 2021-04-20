0c2676d23cd9e9562ee6cad22148d31f2b9b541d
William Johnson <wj637750@southeast.edu>
2021-04-20 14:07:32 -0500

    adding pagination, stuck becuase links dont show up

--- assigment7/Controllers/ProductController.cs
+++ assigment7/Controllers/ProductController.cs
@@ -4,18 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using assigment7.Models;
+using assigment7.Models.ViewModels;
 
 namespace assigment7.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult List() => View(repository.Products);
+        public ViewResult List(int productPage = 1)
+            => View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((productPage - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
     }
 }
---
+++ assigment7/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace assigment7.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
